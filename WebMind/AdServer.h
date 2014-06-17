9d74d597daf309122956fcde25b21eddd8d673a6
Yue Ning <yuefuturening@gmail.com>
2014-06-17 17:45:50 -0400

    Update AdServer.h

--- WebMind/AdServer.h
+++ WebMind/AdServer.h
@@ -1,10 +1,6 @@
 ///////////////////////////////////////////////////////////////
 // AdServer.h: interface for the CAdServer class.
 //
-// Copyright (C) 2000-2004 Tingshao Zhu. All rights reserved!
-// Use in commercial applications requires written permission
-// This software is provided "as is", with no warranty.
-//
 ///////////////////////////////////////////////////////////////
 
 #if !defined(AFX_ADSERVER_H__C0BD7794_66A0_48FB_B50E_51C9F9E9E484__INCLUDED_)
