bdcabc009bd2a325205f425ca4509572d34b8617
Yue Ning <yuefuturening@gmail.com>
2014-06-17 17:56:18 -0400

    Update ChineseCode.h

--- WebMind/ChineseCode.h
+++ WebMind/ChineseCode.h
@@ -14,12 +14,12 @@ using namespace std;
 class ChineseCode
 {
    public:
-       static void UTF_8ToUnicode(wchar_t* pOut,char *pText);  // 把UTF-8转换成Unicode
-       static void UnicodeToUTF_8(char* pOut,CString pText); //Unicode 转换成UTF-8
-       static void UnicodeToGB2312(char* pOut,wchar_t uData);  // 把Unicode 转换成 GB2312 
-       static void Gb2312ToUnicode(wchar_t* pOut,char *gbBuffer);// GB2312 转换成　Unicode
-      // static void GB2312ToUTF_8(string& pOut,char *pText, int pLen);//GB2312 转为 UTF-8
-       static void UTF_8ToGB2312(string &pOut, char *pText, int pLen);//UTF-8 转为 GB2312
+       static void UTF_8ToUnicode(wchar_t* pOut,char *pText);  
+       static void UnicodeToUTF_8(char* pOut,CString pText); 
+       static void UnicodeToGB2312(char* pOut,wchar_t uData); 
+       static void Gb2312ToUnicode(wchar_t* pOut,char *gbBuffer);
+      // static void GB2312ToUTF_8(string& pOut,char *pText, int pLen);
+       static void UTF_8ToGB2312(string &pOut, char *pText, int pLen);
 };
 
 #endif // !defined(AFX_CHINESECODE_H__E9206178_CA96_4B8A_AB2A_AD94C9CD12B1__INCLUDED_)
